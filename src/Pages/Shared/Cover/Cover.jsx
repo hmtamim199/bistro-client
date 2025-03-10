847e17397448ba445b61574a5aeead8446f2c66c
hmtamim199 <hmtamim8906@gmail.com>
2025-03-10 22:42:19 +0600

    dynamic routes added

--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -11,11 +11,7 @@ const Cover = ({ img, title }) => {
         <div className="hero-content text-neutral-content text-center">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+            <p className="mb-5">this is the beautiful and delicious food</p>
           </div>
         </div>
       </div>
